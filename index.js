858a8200d9fe9d8b9a27f654422fdf6189586e4c
Neco86 <33189641+Neco86@users.noreply.github.com>
2024-12-30 15:09:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-const fs = require("fs");
-const path = require("path");
 const https = require("https");
 
-const getBdCookie = () => {
-    return new Promise((resolve, reject) => {
-        https
-            .get("https://www.baidu.com/", (res) => {
-                const cookie = res.headers["set-cookie"]
-                    .map((str) => str.split(";")[0])
-                    .find((cookie) => /^BAIDUID=/.test(cookie));
-                resolve(cookie || "");
-            })
-            .on("error", (err) => {
-                reject(err);
-            });
-    });
+const getCookie = (generateNew) => {
+    if (generateNew) {
+        return new Promise((resolve, reject) => {
+            https
+                .get("https://www.baidu.com/", (res) => {
+                    const cookie =
+                        res.headers["set-cookie"]
+                            .map((str) => str.split(";")[0])
+                            .find((cookie) => /^BAIDUID=/.test(cookie)) || "";
+                    globalThis.BD_NEW_USER = cookie;
+                    resolve(cookie);
+                })
+                .on("error", (err) => {
+                    reject(err);
+                });
+        });
+    } else if (globalThis.BD_NEW_USER) {
+        return Promise.resolve(globalThis.BD_NEW_USER);
+    }
+    return getCookie(true);
 };
 
-exports.server = (server, options) => {
+exports.rulesServer = (server) => {
     server.on("request", (req, res) => {
         const url = req.url;
         const { ruleValue } = req.originalReq;
-        if (ruleValue && new RegExp(ruleValue).test(url)) {
-            getBdCookie().then((cookie) => {
-                fs.writeFileSync(
-                    path.join(__dirname, "headers.txt"),
-                    `Cookie: ${cookie}`
-                );
-                req.passThrough();
-            });
-        } else {
-            req.passThrough();
-        }
+
+        getCookie(ruleValue && new RegExp(ruleValue).test(url)).then(
+            (cookie) => {
+                const rule = `
+            \`\`\`reqHeaders
+            Cookie: ${cookie}
+            \`\`\`
+            \`\`\`resHeaders
+            Bd-New-User: ${cookie}
+            \`\`\`
+            * reqHeaders://{reqHeaders} resHeaders://{resHeaders}
+            `;
+                res.end(rule);
+            }
+        );
     });
 };
